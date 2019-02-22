328f168125a81a667c020ddcd0c196cb0f98d4d3
Zakaria Charik <z.charik@gmail.com>
2019-02-22 16:44:35 +0100

    Add front scanpage as default

--- www/js/app.js
+++ www/js/app.js
@@ -66,31 +66,53 @@ $$('._button-sign').on('click', function () {
 
 var showQrScanner = function(){
     alert('window.QRscanner from scan page ',window.QRscanner);
-    // Make the webview transparent so the video preview is visible behind it.
-    window.QRScanner.show();
-    // Be sure to make any opaque HTML elements transparent here to avoid
-    // covering the video.    
+  // For the best user experience, make sure the user is ready to give your app
+  // camera access before you show the prompt. On iOS, you only get one chance.
 
-
-    // Start a scan. Scanning will continue until something is detected or
-    // `QRScanner.cancelScan()` is called.
-    window.QRScanner.scan(displayContents);
-     
-    function displayContents(err, text){
-      if(err){
-        // an error occurred, or the scan was canceled (error code `6`)
-      } else {
-        // The scan completed, display the contents of the QR code:
-        alert(text);
-      }
+  window.QRScanner.prepare(  function (err, status){
+    if (err) {
+     // here we can handle errors and clean up any loose ends.
+     console.error(err);
     }
+    if (status.authorized) {
+    
+        // Make the webview transparent so the video preview is visible behind it.
+        window.QRScanner.show();
+        // Be sure to make any opaque HTML elements transparent here to avoid
+        // covering the video.    
+
+
+        // Start a scan. Scanning will continue until something is detected or
+        // `QRScanner.cancelScan()` is called.
+        window.QRScanner.scan(displayContents);
+         
+        function displayContents(err, text){
+          if(err){
+            // an error occurred, or the scan was canceled (error code `6`)
+          } else {
+            // The scan completed, display the contents of the QR code:
+            alert(text);
+          }
+        }
+
+    } else if (status.denied) {
+     // The video preview will remain black, and scanning is disabled. We can
+     // try to ask the user to change their mind, but we'll have to send them
+     // to their device settings with `QRScanner.openSettings()`.
+    } else {
+      // we didn't get permission, but we didn't get permanently denied. (On
+      // Android, a denial isn't permanent unless the user checks the "Don't
+      // ask again" box.) We can ask again at the next relevant opportunity.
+    }
+  }); // show the prompt
+
 };
+
 
 // Option 2. Using live 'page:init' event handlers for each page
 $$(document).on('page:init', '.page[data-name="scan-page"]', function (e) {
 
   document.addEventListener("deviceready", showQrScanner, false);
-
 
 });
 
