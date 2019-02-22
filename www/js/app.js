c96c81569f5a2841ec0b70224af837c02a9c1dfb
Zakaria Charik <z.charik@gmail.com>
2019-02-22 13:02:19 +0100

    Add barcodescanner plugin

--- www/js/app.js
+++ www/js/app.js
@@ -67,29 +67,55 @@ $$('._button-sign').on('click', function () {
 
 // Option 2. Using live 'page:init' event handlers for each page
 $$(document).on('page:init', '.page[data-name="scan-page"]', function (e) {
-  if (window.QRScanner !== undefined) {
-    console.log('window.QRscanner from scan page',window.QRscanner);
-    // Make the webview transparent so the video preview is visible behind it.
-    window.QRScanner.show();
-    // Be sure to make any opaque HTML elements transparent here to avoid
-    // covering the video.    
 
-
-    // Start a scan. Scanning will continue until something is detected or
-    // `QRScanner.cancelScan()` is called.
-    window.QRScanner.scan(displayContents);
-     
-    function displayContents(err, text){
-      if(err){
-        // an error occurred, or the scan was canceled (error code `6`)
-      } else {
-        // The scan completed, display the contents of the QR code:
-        alert(text);
+   cordova.plugins.barcodeScanner.scan(
+      function (result) {
+          alert("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);
+      },
+      function (error) {
+          alert("Scanning failed: " + error);
+      },
+      {
+          preferFrontCamera : true, // iOS and Android
+          showFlipCameraButton : true, // iOS and Android
+          showTorchButton : true, // iOS and Android
+          torchOn: true, // Android, launch with the torch switched on (if available)
+          saveHistory: true, // Android, save scan history (default false)
+          prompt : "Place a barcode inside the scan area", // Android
+          resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+          formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+          orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
+          disableAnimations : true, // iOS
+          disableSuccessBeep: false // iOS and Android
       }
-    }
+   );
+
+  // if (window.QRScanner !== undefined) {
+  //   console.log('window.QRscanner from scan page',window.QRscanner);
+  //   // Make the webview transparent so the video preview is visible behind it.
+  //   window.QRScanner.show();
+  //   // Be sure to make any opaque HTML elements transparent here to avoid
+  //   // covering the video.    
+
+
+  //   // Start a scan. Scanning will continue until something is detected or
+  //   // `QRScanner.cancelScan()` is called.
+  //   window.QRScanner.scan(displayContents);
      
-  }else{
-    alert('QRScanner undefined');
-  }
+  //   function displayContents(err, text){
+  //     if(err){
+  //       // an error occurred, or the scan was canceled (error code `6`)
+  //     } else {
+  //       // The scan completed, display the contents of the QR code:
+  //       alert(text);
+  //     }
+  //   }
+     
+  // }else{
+  //   alert('QRScanner undefined');
+  // }
 })
 
