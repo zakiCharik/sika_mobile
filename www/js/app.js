6381e75d4ae8b41e28e9762d79ab19de74006f43
Zakaria Charik <z.charik@gmail.com>
2019-02-22 12:41:02 +0100

    Set ap the scan action

--- www/js/app.js
+++ www/js/app.js
@@ -73,6 +73,25 @@ $$(document).on('page:init', '.page[data-name="scan-page"]', function (e) {
     window.QRScanner.show();
     // Be sure to make any opaque HTML elements transparent here to avoid
     // covering the video.    
+
+    
+    // Start a scan. Scanning will continue until something is detected or
+    // `QRScanner.cancelScan()` is called.
+    window.QRScanner.scan(displayContents);
+     
+    function displayContents(err, text){
+      if(err){
+        // an error occurred, or the scan was canceled (error code `6`)
+      } else {
+        // The scan completed, display the contents of the QR code:
+        alert(text);
+      }
+    }
+     
+    // Make the webview transparent so the video preview is visible behind it.
+    window.QRScanner.show();
+    // Be sure to make any opaque HTML elements transparent here to avoid
+    // covering the video.
   }
 })
 
