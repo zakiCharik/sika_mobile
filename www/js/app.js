a7291bb6ef564e81047f3733017a99f86c8b44df
Zakaria Charik <z.charik@gmail.com>
2019-02-25 10:11:44 +0100

    added the scan barcode function

--- www/js/app.js
+++ www/js/app.js
@@ -66,45 +66,50 @@ $$('._button-sign').on('click', function () {
 
 var showQrScanner = function(){
     alert('window.QRscanner from scan page ',window.QRscanner);
+    cordova.plugins.barcodeScanner.scan(function(result){console.log(result), function(err){alert(err)} })
+
   // For the best user experience, make sure the user is ready to give your app
   // camera access before you show the prompt. On iOS, you only get one chance.
 
-  window.QRScanner.prepare(  function (err, status){
-    if (err) {
-     // here we can handle errors and clean up any loose ends.
-     console.error(err);
-    }
-    if (status.authorized) {
+  // window.QRScanner.prepare(onDone); // show the prompt
+
+  // function onDone(err, status){
+  //   if (err) {
+  //    // here we can handle errors and clean up any loose ends.
+  //    console.error(err);
+  //   }
+  //   if (status.authorized) {
     
-        // Make the webview transparent so the video preview is visible behind it.
-        window.QRScanner.show();
-        // Be sure to make any opaque HTML elements transparent here to avoid
-        // covering the video.    
+  //       // Make the webview transparent so the video preview is visible behind it.
+  //       window.QRScanner.show();
+  //       // Be sure to make any opaque HTML elements transparent here to avoid
+  //       // covering the video.    
 
 
-        // Start a scan. Scanning will continue until something is detected or
-        // `QRScanner.cancelScan()` is called.
-        window.QRScanner.scan(displayContents);
+  //       // Start a scan. Scanning will continue until something is detected or
+  //       // `QRScanner.cancelScan()` is called.
+  //       window.QRScanner.scan(displayContents);
          
-        function displayContents(err, text){
-          if(err){
-            // an error occurred, or the scan was canceled (error code `6`)
-          } else {
-            // The scan completed, display the contents of the QR code:
-            alert(text);
-          }
-        }
+  //       function displayContents(err, text){
+  //         if(err){
+  //           // an error occurred, or the scan was canceled (error code `6`)
+  //         } else {
+  //           // The scan completed, display the contents of the QR code:
+  //           alert(text);
+  //         }
+  //       }
 
-    } else if (status.denied) {
-     // The video preview will remain black, and scanning is disabled. We can
-     // try to ask the user to change their mind, but we'll have to send them
-     // to their device settings with `QRScanner.openSettings()`.
-    } else {
-      // we didn't get permission, but we didn't get permanently denied. (On
-      // Android, a denial isn't permanent unless the user checks the "Don't
-      // ask again" box.) We can ask again at the next relevant opportunity.
-    }
-  }); // show the prompt
+  //   } else if (status.denied) {
+  //    // The video preview will remain black, and scanning is disabled. We can
+  //    // try to ask the user to change their mind, but we'll have to send them
+  //    // to their device settings with `QRScanner.openSettings()`.
+  //   } else {
+  //     // we didn't get permission, but we didn't get permanently denied. (On
+  //     // Android, a denial isn't permanent unless the user checks the "Don't
+  //     // ask again" box.) We can ask again at the next relevant opportunity.
+  //   }
+  // }
+
 
 };
 
@@ -112,7 +117,9 @@ var showQrScanner = function(){
 // Option 2. Using live 'page:init' event handlers for each page
 $$(document).on('page:init', '.page[data-name="scan-page"]', function (e) {
 
+
   document.addEventListener("deviceready", showQrScanner, false);
+
 
 });
 
--- www/js/qrscanner.js
+++ www/js/qrscanner.js
@@ -1,17 +1,17 @@
 
 
 
-var displayContents = function(err, text){
-  if(err){
-    // an error occurred, or the scan was canceled (error code `6`)
-    alert('an error occurred, or the scan was canceled : '+err);
-    console.log(err);
-  } else {
-    // The scan completed, display the contents of the QR code:
-    alert('SCANNED content : '+text);
-    console.log(text);
-  }
-}
+// var displayContents = function(err, text){
+//   if(err){
+//     // an error occurred, or the scan was canceled (error code `6`)
+//     alert('an error occurred, or the scan was canceled : '+err);
+//     console.log(err);
+//   } else {
+//     // The scan completed, display the contents of the QR code:
+//     alert('SCANNED content : '+text);
+//     console.log(text);
+//   }
+// }
 
 
 
